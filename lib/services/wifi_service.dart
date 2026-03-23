49a28d65997fd0a271bb1bad94691d89310d2a01
Anil Tigga <anil.anil300@gmail.com>
2026-03-23 23:16:27 +0530

    Update wifi_service.dart

--- lib/services/wifi_service.dart
+++ lib/services/wifi_service.dart
@@ -1,17 +1,18 @@
 import 'package:network_info_plus/network_info_plus.dart';
 
 class WifiService {
-  static const String officeBSSID = "70:97:41:da:12:d4";
+  static const String officeBSSID = "82:97:41:da:12:dc";
 
   static Future<bool> isConnectedToOffice() async {
     final info = NetworkInfo();
+    // print("SSID: ${await info.getWifiName()}");
+    // print("BSSID: ${await info.getWifiBSSID()}");
 
     try {
       String? bssid = await info.getWifiBSSID();
 
       if (bssid == null) return false;
 
-    print('Attendance MArked');
       return bssid.toLowerCase() == officeBSSID.toLowerCase();
     } catch (e) {
       return false;
